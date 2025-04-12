0932f315ac5dd50bf371f4f49bb935f8fd272fc7
Valame, Tejas <tejasvalame@gmail.com>
2025-04-12 11:41:35 +0530

    Added Logging Added get Currency API

--- src/main/java/com/example/demo/controller/RestService.java
+++ src/main/java/com/example/demo/controller/RestService.java
@@ -1,21 +1,45 @@
 package com.example.demo.controller;
 
+import org.oorsprong.websamples.ArrayOftCurrency;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
+@ApiResponses({
+        @ApiResponse(responseCode = "404", content = { @Content(mediaType = MediaType.TEXT_PLAIN_VALUE) }),
+        @ApiResponse(responseCode = "500", content = { @Content(mediaType = MediaType.TEXT_PLAIN_VALUE) })
+})
 public interface RestService {
 
     @GetMapping("/capital/{countryISOCode}")
-    @ApiResponse(responseCode = "200", content = {
-            @Content(mediaType = MediaType.TEXT_PLAIN_VALUE, schema = @Schema(implementation = String.class)) })
-    public ResponseEntity<?> getCapital(@PathVariable(name = "countryISOCode") String countryISOCode);
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(mediaType = MediaType.TEXT_PLAIN_VALUE) }),
+    })
+
+    public ResponseEntity<?> getCapital(@NonNull @PathVariable(name = "countryISOCode") String countryISOCode);
+
+    @GetMapping("/currency")
+    @Parameters(@Parameter(name = "sCurrencyISOCode", in = ParameterIn.QUERY, required = false))
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = ArrayOftCurrency.class)) }),
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = String.class)) }) })
+
+    public ResponseEntity<?> getCurrency(
+            @RequestParam(name = "sCurrencyISOCode", required = false) String sCurrencyISOCode);
 
 }
--- src/main/java/com/example/demo/controller/RestServiceImpl.java
+++ src/main/java/com/example/demo/controller/RestServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.oorsprong.websamples.CountryInfoServiceSoapType;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -10,12 +12,53 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class RestServiceImpl implements RestService{
+public class RestServiceImpl implements RestService {
 
-        private final CountryInfoServiceSoapType service;
+    private final CountryInfoServiceSoapType service;
 
-        @Override
-        public ResponseEntity<?> getCapital(String countryISOCode) {
-            return ResponseEntity.ok().body(service.capitalCity(countryISOCode));
+    @Override
+    public ResponseEntity<?> getCapital(@NonNull String countryISOCode) {
+
+        ResponseEntity<?> response;
+        String capital;
+
+        log.debug("Searching capital for country {}", countryISOCode);
+
+        if (countryISOCode == null || countryISOCode.strip().isEmpty()) {
+            response = ResponseEntity.internalServerError().body("Country Code cannot be null or empty");
+        } else {
+            capital = service.capitalCity(countryISOCode);
+            log.info("getCapital API returned response \"{}\" for country: {}", capital, countryISOCode);
+
+            if ("Country not found in the database".equalsIgnoreCase(capital.strip())) {
+                response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(capital);
+            } else {
+                response = ResponseEntity.ok().body(capital);
+            }
         }
+
+        log.debug("Returning response \"{}\" with HTTP Status Code: {}", response.getBody(), response.getStatusCode());
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<?> getCurrency(String sCurrencyISOCode) {
+
+        ResponseEntity<?> response;
+        String currency;
+
+        if (sCurrencyISOCode == null || sCurrencyISOCode.strip().isEmpty()) {
+            response = ResponseEntity.ok().body(service.listOfCurrenciesByName());
+        } else {
+            currency = service.currencyName(sCurrencyISOCode);
+            if ("Currency code NOT found".equalsIgnoreCase(currency.strip())) {
+                response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(currency);
+            } else {
+                response = ResponseEntity.ok().body(currency);
+            }
+        }
+        log.debug("Returning response \"{}\" with HTTP Status Code: {}", response.getBody(), response.getStatusCode());
+        return response;
+
+    }
 }
